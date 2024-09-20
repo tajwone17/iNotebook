bda48bdabf70dc343d3a44135658046757b73ae0
Jakaria Chowdhury Tajwone <0562310005101031@neub.edu.bd>
2024-09-21 00:48:20 +0600

    iNotebook db & Express Server Setup

---
+++ backend/db.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const mongoURI = "mongodb://localhost:27017";
+
+const connectToMongo = async () => {
+    try {
+        await mongoose.connect(mongoURI);
+        console.log("Connected to MongoDB");
+    } catch (error) {
+        console.error("Could not connect to MongoDB:", error);
+    }
+};
+// const connectToMongo=()=>{
+//     mongoose.connect(mongoURI,()=>{
+//         console.log("Connected");
+//     })
+//     }
+
+module.exports = connectToMongo;
