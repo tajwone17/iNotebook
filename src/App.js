8bc89ba4412cd9836a2d220f0f208672609d4be6
Jakaria Chowdhury Tajwone <0562310005101031@neub.edu.bd>
2024-10-10 00:42:38 +0600

    navbar updated

--- src/App.js
+++ src/App.js
@@ -3,17 +3,22 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Components/Home/Home";
 import About from "./Components/About/About";
 import Navbar from "./Components/Navbar/Navbar";
+import NoteState from "./context/notes/NoteState";
 
 function App() {
   return (
     <>
+    <NoteState>
       <Router>
         <Navbar />
+        {/* <div className="container"> */}
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
         </Routes>
+        {/* </div> */}
       </Router>
+      </NoteState>
     </>
   );
 }
